44274cdfec07714965dac5b74a6167fe4d6cff6b
arturofgg <arturofdezgg17@gmail.com>
2023-04-26 12:12:39 +0200

    cambio modelos, imagen, video y puntuacion

--- vimad/vimad_app/models.py
+++ vimad/vimad_app/models.py
@@ -27,15 +27,15 @@ class Actor(models.Model):
     
 class Corto(models.Model):
     titulo=models.CharField(max_length=50)
-    puntuacion=models.CharField(max_length=10)
+    puntuacion=models.IntegerField(max_length=10)
     genero=models.CharField(max_length=50)
     duracion=models.CharField(max_length=3)
     fec_estreno=models.DateField()
     idioma=models.CharField(max_length=50)
     pais=models.CharField(max_length=50)
     sinopsis=models.TextField(max_length=50)
-    imagen=models.ImageField(upload_to='', null=True)
-    video=models.FileField(upload_to='',null=True)
+    imagen=models.CharField()
+    video=models.CharField()
     director = models.ManyToManyField(Director, through='Dirige')
     actor = models.ManyToManyField(Actor, through='Actua')
     estudio = models.ForeignKey(Estudio, on_delete=models.CASCADE)
