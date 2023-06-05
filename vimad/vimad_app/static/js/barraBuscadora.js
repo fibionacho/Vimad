87da3f3abb2f8efb31350f9f04375f48f8856531
arturofgg <arturofdezgg17@gmail.com>
2023-06-06 01:22:19 +0200

    barra buscadora fin, reset email

--- vimad/vimad_app/static/js/barraBuscadora.js
+++ vimad/vimad_app/static/js/barraBuscadora.js
@@ -1,25 +1,26 @@
 $(document).ready(function() {
     $('#buscar-btn').click(function() {
-        var query = $('#buscar-input').val();
-
-        $.ajax({
-            url: '/buscar/',
-            data: {
-                'q': query
-            },
-            dataType: 'json',
-            success: function(data) {
-                $('#resultados-busqueda').empty();
-
-                if (data.cortos.length == 0) {
-                    $('#resultados-busqueda').append('<p>No se encontraron resultados.</p>');
-                } else {
-                    $.each(data.cortos, function(index, corto) {
-                        $('#resultados-busqueda').append('<a href="ficha/' + corto.slug + '" class="list-group-item list-group-item-action"><img src="' + corto.imagen + '" alt="Imagen del corto" width="300px"><h5 class="mb-1">' + corto.titulo + '</h5><p class="mb-1">' + corto.genero + ', ' + corto.idioma + ', ' + corto.pais + '</p></a>');
-
-                    });
-                }
-            }
-        });
+      var query = $('#buscar-input').val();
+  
+      $.ajax({
+        url: '/buscar/',
+        data: {
+          'q': query
+        },
+        dataType: 'json',
+        success: function(data) {
+          $('#resultados-busqueda').empty();
+  
+          if (data.cortos.length == 0) {
+            $('#resultados-busqueda').append('<p>No se encontraron resultados.</p>');
+          } else {
+            $.each(data.cortos, function(index, corto) {
+              var urlFicha = '/ficha/' + corto.slug;
+              $('#resultados-busqueda').append('<a href="' + urlFicha + '" class="list-group-item list-group-item-action"><img src="' + corto.imagen + '" alt="Imagen del corto" width="300px"><h5 class="mb-1">' + corto.titulo + '</h5><p class="mb-1">' + corto.genero + ', ' + corto.idioma + ', ' + corto.pais + '</p></a>');
+            });
+          }
+        }
+      });
     });
-});
+  });
+  
